699b2000b333cb1ac3651c70c7b0a72b654cdb2c
kon104 <tkonuma@R-SUR-L4BL-0559.localdomain>
2023-08-15 20:56:32 +0900

    added the sub menu to check a stock info by kabu yoho and minkabu

--- stockcodeviewer/src/background.js
+++ stockcodeviewer/src/background.js
@@ -22,6 +22,24 @@ chrome.runtime.onInstalled.addListener(function (details) {
 		title: "Yahoo! ファイナンス",
 		contexts: ["all"],
 	});
+	chrome.contextMenus.create({
+		parentId: parent,
+		type: "separator",
+		id: "seperator01",
+		contexts: ["all"],
+	});
+	chrome.contextMenus.create({
+		parentId: parent,
+		id: "kabuyohoGoal",
+		title: "株予報：目標株価",
+		contexts: ["all"],
+	});
+	chrome.contextMenus.create({
+		parentId: parent,
+		id: "minkabuPrediction",
+		title: "みんかぶ：株価予想",
+		contexts: ["all"],
+	});
 });
 
 
@@ -29,10 +47,16 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
 	let baseurl = '';
 	switch (info.menuItemId) {
 		case "kabutanChart":
-			baseurl = 'https://kabutan.jp/stock/chart?code=';
+			baseurl = 'https://kabutan.jp/stock/chart?code=%s';
 			break;
 		case "yjfChart":
-			baseurl = 'https://finance.yahoo.co.jp/search/?query=';
+			baseurl = 'https://finance.yahoo.co.jp/search/?query=%s';
+			break;
+		case "kabuyohoGoal":
+			baseurl = 'https://kabuyoho.jp/sp/reportTarget?bcode=%s';
+			break;
+		case "minkabuPrediction":
+			baseurl = 'https://minkabu.jp/stock/%s/analysis';
 			break;
 	}
 	chrome.scripting.executeScript({
@@ -51,7 +75,8 @@ function gotoUrlWithDraggedText(baseurl) {
 		}
 	);
 	console.log("document.getSelection() = [" + text + "]");
-	let url = baseurl + text;
+//	let url = baseurl + text;
+	let url = baseurl.replace("%s", text);
 	url = encodeURI(url);
 	window.open(url, '_blank');
 }
